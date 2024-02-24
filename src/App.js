3e737a3735f437f21747540430203a74ea1caf8b
spaghetta <katharinadaun@bluewin.ch>
2024-02-24 17:56:35 +0100

    Add Tic Tac Toe board and enable marking squares

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+function Square({key, xIsNext, setXIsNext}) {
+  const [value, setValue] = useState("");
+  function handleClick() {
+    if (value !== "X" && value !== "O") {
+      if (xIsNext) {
+        setValue("X");
+      } else {
+        setValue("O");
+      }
+      setXIsNext(!xIsNext);
+    }
+  }
+  return <button key={key} className="square" onClick={handleClick}>{value}</button>
+}
+
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  return (
+  <>
+  {
+    [1,2,3].map(row => <div className="board-row">
+      {
+      [1,2,3].map(col => <Square xIsNext={xIsNext} setXIsNext={setXIsNext} key={3*(row-1) + col}/>)
+      }
+  </div>)
+  }
+  </>
+  );
 }
