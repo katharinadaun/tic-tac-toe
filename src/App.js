a4d5580843f2b079da7dc03f3d4f977925d1b56d
spaghetta <katharinadaun@bluewin.ch>
2024-02-25 14:33:19 +0100

    Enable going back to previous moves

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ function Square({value, handleClick}) {
   return <button className="square" onClick={handleClick}>{value}</button>
 }
 
-export default function Board() {
-  const[squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState(true);
-
-  const winner = calculateWinner();
+function Board({ xIsNext, squares, onPlay }) {
+  const winner = calculateWinner(squares);
   let status;
   if (winner) {
     status = "Winner: " + winner;
@@ -16,7 +13,7 @@ export default function Board() {
     status = "Next player: " + (xIsNext? "X" : "O");
   }
   function handleClick(index) {
-    if (squares[index] || calculateWinner()) {
+    if (squares[index] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -25,31 +22,12 @@ export default function Board() {
     } else {
       nextSquares[index] = "O"
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
-  function calculateWinner() {
-    const winningNums = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,4,8],
-      [2,4,6],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8]  
-    ];
-    for (let i = 0; i < winningNums.length; i++) {
-      if (squares[winningNums[i][0]] === squares[winningNums[i][1]] && squares[winningNums[i][0]] === squares[winningNums[i][2]]){
-        return squares[winningNums[i][0]];
-      }
-    }
-    return null;
-  }
   return (
   <>
-  <div>{status}</div>
+  <div className="status">{status}</div>
   {
     [0,1,2].map(row => <div key={row} className="board-row">
       {
@@ -59,4 +37,61 @@ export default function Board() {
   }
   </>
   );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const currentSquares = history[history.length-1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(index) {
+    setHistory(history.slice(0,index+1));
+    setXIsNext(index % 2 == 0)
+  }
+  // variables starting with _ will not be checked if they are being used
+  const moves = history.map((_square, index) => {
+    let description = index > 0? 'Go to move ' + index : 'Go to game start';
+    // fine to use index as key here because array will never be reordered
+    return (
+    <li key={index}>
+      <button onClick={() => jumpTo(index)}>{description}</button>
+    </li>
+    )
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
+
+function calculateWinner(squares) {
+  const winningNums = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]  
+  ];
+  for (let i = 0; i < winningNums.length; i++) {
+    const [a,b,c] = winningNums[i];
+    if (squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
 }
